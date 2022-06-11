462dba5f6f9ab6f7748a2d92b8d4e7c1f3676ef9
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-11 08:05:35 -0600

    Add project files.

---
+++ CommissionEmployee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesChp11
+{
+    public class CommissionEmployee : object
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+        private decimal grossSales;
+        private decimal commissionRate;
+
+        // Five parameter constructor
+        public CommissionEmployee(string firstName, string lastName, 
+            string socialSecurityNumber, decimal grossSales, decimal commissionRate)
+        {
+            // Implicit call to object constructor occurs here
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+            GrossSales = grossSales; // Validates gross sales
+            CommissionRate = commissionRate; // Validates commission rate
+        }
+
+        // Property that gets and sets commission employee's gross sales
+        public decimal GrossSales
+        {
+            get
+            {
+                return grossSales;
+            }
+            set
+            {
+                if (value < 0 ) // Validation -- the "check for this so and so
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), 
+                        value, $"{nameof(GrossSales)} must be >= 0");
+                }
+
+                grossSales = value;
+            }
+        }
+
+        // Property that gets and sets commission employee's commission rate
+        public decimal CommissionRate
+        {
+            get
+            {
+                return commissionRate;
+            }
+            set
+            {
+                if (value <= 0 || value >= 1)  // Validation of not less the 0% or higher than 100% or 1.0
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(CommissionRate)} must be greater than 0 and less than 1");
+                }
+
+                commissionRate = value;
+            }
+        }
+
+        // Calculate commission employee's pay
+        public decimal Earnings() => commissionRate * grossSales;
+
+        // Return string representation of CommissionEmployee object
+        public override string ToString() =>
+            $"Commission employee:  {FirstName} {LastName}\n" +
+            $"Social security number: {SocialSecurityNumber}\n" +
+            $"Gross sales: {GrossSales:C}\n" +
+            $"Commission rate: {CommissionRate:F2}";
+    }
+}
---
+++ CommissionEmployeeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EmployeesChp11
+{
+    class CommissionEmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate CommissionEmployee object
+            var employee = new CommissionEmployee("Seven", "of Nine",
+                "777-77-7777", 7000.00M, 0.07M);
+
+            // Display CommissionEmployee data
+            Console.WriteLine(
+                "Employee information obtained by properties and methods: \n");
+            Console.WriteLine($"First name is {employee.FirstName}");
+            Console.WriteLine($"Last name is {employee.LastName}");
+            Console.WriteLine($"Social security number is {employee.SocialSecurityNumber}");
+            Console.WriteLine($"Gross sales are {employee.GrossSales}");
+            Console.WriteLine($"Commission rate is {employee.CommissionRate:F2}");
+            Console.WriteLine($"Earnings are {employee.Earnings():C}");
+
+            employee.GrossSales = 7000000.00M;  // Update gross sales
+            employee.CommissionRate = 0.70M; // Update commission rate
+
+            Console.WriteLine("\nUpdated employee information obtained by ToString:\n");
+            Console.WriteLine(employee);
+            Console.WriteLine($"Earnings: {employee.Earnings():C}");
+        }
+    }
+}
