da8f372090765e3d3ca2fe2f292ee0da50bc0dd5
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-06-14 14:57:38 -0600

    feat: Added SalariedEmployee class that extends Employee abstract base class

---
+++ Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesChp11
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string SocialSecurityNumber { get; }
+    
+        // Three-parameter constructor
+        public Employee(string firstName, string lastName, 
+            string socialSecurityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecurityNumber;
+        }
+
+        // Return string representation of Eployee object, using properties
+        public override string ToString() => $"{FirstName} {LastName}\n" +
+            $"Social security number: {SocialSecurityNumber}";
+
+        // Abstract method overridden by derived classes
+        public abstract decimal Earnings();  // No implementation here
+    }
+}
---
+++ SalariedEmployee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesChp11
+{
+    public class SalariedEmployee : Employee
+    {
+        private decimal weeklySalary;
+
+        // Four-parameter constructor
+        public SalariedEmployee(string firstName, string lastName,
+            string socialSecurityNumber, decimal weeklySalary)
+            : base(firstName, lastName, socialSecurityNumber)
+        {
+            WeeklySalary = weeklySalary;
+        }
+
+        // Property that gets and sets salaried employee's salary
+        public decimal WeeklySalary
+        {
+            get
+            {
+                return weeklySalary;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(WeeklySalary)} must >= 0");
+                }
+
+                weeklySalary = value;
+            }
+        }
+
+        // Calculate earnings. overrride abstract method Earnings in Employee
+        public override decimal Earnings() => WeeklySalary;
+
+        // Return string representation 
+        public override string ToString() =>
+            $"Salaried employee: {base.ToString()}\n" +
+            $"Weekly salary: {WeeklySalary:C}";
+    }
+}
