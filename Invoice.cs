c35c87706c2183b35f69a0e906e492c9b0e7b1e6
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-06-23 17:58:40 -0600

    feat: Added class Invoice

---
+++ Invoice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesChp11
+{
+    public class Invoice : IPayable
+    {
+        public string PartNumber { get; }
+        public string PartDescription { get; }
+        private int quantity;
+        private decimal pricePerItem;
+
+        // Four parameter constructor
+        public Invoice(string partNumber, string partDescription, int quantity, 
+            decimal pricePerItem)
+        {
+            PartNumber = partNumber;
+            PartDescription = partDescription;
+            Quantity = quantity;
+            PricePerItem = pricePerItem;
+        }
+
+        // Property to get quantity in invoice
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                          value, $"{nameof(Quantity)} must >= 0");
+                }
+
+                quantity = value;
+            }
+        }
+
+        // Property to get set PricePerItem in invoice
+        public decimal PricePerItem
+        {
+            get
+            {
+                return pricePerItem;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                          value, $"{nameof(PricePerItem)} must >= 0");
+                }
+
+                pricePerItem = value;
+            }
+        }
+
+        // Return string representation of Invoice object
+        public override string ToString() =>
+            $"Invoice:\nPart number: {PartNumber} ({PartDescription})" +
+            $"Quantity: {Quantity}\nPrice per item: {PricePerItem}";
+
+        // Method required to carry out contract with interface IPayable
+        public decimal GetPaymentAmount() => Quantity * PricePerItem;
+    }
+}
